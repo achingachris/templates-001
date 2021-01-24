7aae1bd7977c1187f8c856dd97389dfc53ee147c
ChrisAchinga <achinga.chris@gmail.com>
2021-01-24 23:43:27 +0300

    use usestate to render tasks list

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,26 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'doctor check',
-    day: 'Jan 32th at 8am',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'fact check',
-    day: 'Feb 32th at 8am',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'code check',
-    day: 'Dev 32th at 8am',
-    reminder: false,
-  },
-]
+import { useState } from 'react'
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'doctor check',
+      day: 'Jan 32th at 8am',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'fact check',
+      day: 'Feb 32th at 8am',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'code check',
+      day: 'Dev 32th at 8am',
+      reminder: false,
+    }
+  ])
   return (
     <>
       {tasks.map((task) => (
