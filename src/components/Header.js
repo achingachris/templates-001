3a171190409975febbf2e935b2c15e31c60c6637
ChrisAchinga <achinga.chris@gmail.com>
2021-01-24 22:56:14 +0300

    setup

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+
+
+const Header = ({ title }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  )
+}
+
+// default props
+Header.defaultProps = {
+  title: 'Header',
+}
+
+// prop types
+Header.propTypes = {
+    title: PropTypes.string,
+}
+
+export default Header
