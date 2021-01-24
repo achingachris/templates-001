172124495d7f9a7a2fb6062a28d897e81273d603
ChrisAchinga <achinga.chris@gmail.com>
2021-01-24 23:16:42 +0300

    styles - external

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
   return (
     <header>
-      <h1 style={headStyle}>{title}</h1>
+      <h1>{title}</h1>
     </header>
   )
 }
 
-// styles
-const headStyle = {
-  color: 'red',
-  backgroundColor: 'black',
-}
+// styles in js
+// const headStyle = {
+//   color: 'red',
+//   backgroundColor: 'black',
+// }
 
 // default props
 Header.defaultProps = {
